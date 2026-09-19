27c5b3b3e8e56f59e4af4278c9d53f64bea56de4
Alazhar Ashalli <store20626@gmail.com>
2026-09-19 10:33:17 +0400

    Create CalendarEventColor.kt

---
+++ app/src/main/java/om/squ/roverscalendar/model/CalendarEventColor.kt
@@ -0,0 +1,11 @@
+package om.squ.roverscalendar.model
+
+import androidx.compose.ui.graphics.Color
+
+enum class CalendarEventColor(val hex: String, val color: Color) {
+    NAVY("#0B1B3D", Color(0xFF0B1B3D)),
+    GOLD("#C5A059", Color(0xFFC5A059)),
+    RED("#D32F2F", Color(0xFFD32F2F)),
+    GREEN("#388E3C", Color(0xFF388E3C)),
+    BLUE("#1976D2", Color(0xFF1976D2))
+}
