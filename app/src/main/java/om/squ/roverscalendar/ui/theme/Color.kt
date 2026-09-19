32825e204705bdd86411fdd08de21fba0be1b98a
Alazhar Ashalli <store20626@gmail.com>
2026-09-19 10:23:59 +0400

    Add files via upload

---
+++ app/src/main/java/om/squ/roverscalendar/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package om.squ.roverscalendar.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val NavyPrimary = Color(0xFF0B1B3D)
+val GoldAccent = Color(0xFFC5A059)
+val CardGold = Color(0xFFD4AF37)
+val TableHeaderBlue = Color(0xFF0C2340)
